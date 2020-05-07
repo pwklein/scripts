db75e2a2ce891b20d882bc4125728901c0add0c0
pwklein <philipp.klein@polytechnique.edu>
2020-05-07 21:19:59 +0200

    env files added and save module changed

--- save_cpht.sh
+++ save_cpht.sh
@@ -8,15 +8,15 @@ reset=$(tput sgr0);
 
 echo "${color2}*** Saving to CPHT started ${reset}"
 
-echo "${color1}$(rsync -avzhut --progress --delete --dry-run ~/ casimir8:~/home_save_desktop/ | grep deleting  )"
+# echo "${color1}$(rsync -avzhut --progress --delete --dry-run ~/ casimir8:~/home_save_desktop/ | grep deleting  )"
 
-echo "${reset}(to be deleted at CPHT) ${reset}"
+# echo "${reset}(to be deleted at CPHT) ${reset}"
 echo "${color2}*** Do you wish to continue saving to CPHT? [yes/no] ${reset}"
 
 read answer
 
 if [ "$answer" = "yes" ]; then
-        rsync -avzhut --progress --delete ~/ casimir8:~/home_save_desktop/
+        rsync -aAXvhut --progress --delete --exclude=.cache/* ~/ casimir8:~/home_save_desktop/
         echo "${color2}*** Saving to CPHT completed${reset}"
 else
         echo "${color2}*** Saving to CPHT cancelled${reset}"
