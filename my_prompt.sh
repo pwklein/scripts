ff82ab319b6baff89554a3564e76368c39b9b117
pwklein <philipp.klein@polytechnique.edu>
2020-05-10 03:49:29 +0200

    renaming and minor changes

--- my_prompt.sh
+++ my_prompt.sh
@@ -6,11 +6,11 @@ color1=$(tput setaf 178)
 color2=$(tput setaf 228);
 color3=$(tput setaf 15);
 reset=$(tput sgr0);
-
+color4=$(tput setaf 136);
 git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-PS1="\[${color1}\]\u\[${color2}\]{\[${color2}\]\W}\[${color0}\]\$(git_branch)\[${reset}\] $ \[${reset}\]";
+PS1="[\[${color4}\]\u\[${reset}\]@\[${color1}\]\h\[${reset}\]]{\[${color2}\]\W\[${reset}\]}\[${color0}\]\$(git_branch)\[${reset}\] $ ";
 
 export PS1
