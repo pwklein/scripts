9103c1440a35e3a25b25b7efaa46f440a5a5e4f3
pwklein <philipp.klein@polytechnique.edu>
2020-05-27 19:06:25 +0200

    small adjustments

--- backup.sh
+++ backup.sh
@@ -6,16 +6,12 @@ reset=$(tput sgr0);
 
 
 echo "${color2}*** Backup to CPHT started ${reset}"
-
-echo "${color1}$(rsync -avzh --dry-run --progress --exclude='.*' --delete --backup --backup-dir='x_date_$(date +\\%Y-\\%m-\\%d)_time_$(date +\\%H-\\%M)' /home/pwk casimir8:~/backup/  | grep deleting  )"
-
-echo "${reset}(to be deleted at CPHT -> backup directory) ${reset}"
 echo "${color2}*** Do you wish to continue with the backup? [yes/no] ${reset}"
  
 read answer
 
 if [ "$answer" = "yes" ]; then
-        rsync -avzh --progress --exclude=".*" --delete --backup --backup-dir="x_date_$(date +\\%Y-\\%m-\\%d)_time_$(date +\\%H-\\%M)" /home/pwk casimir8:~/backup/
+        rsync -aAXvhut --progress --exclude=.cache/* --delete --backup --backup-dir="x_date_$(date +\\%Y-\\%m-\\%d)_time_$(date +\\%H-\\%M)" /home/pwk casimir8:~/work/
         echo "${color2}*** Backup to CPHT completed${reset}"
 else
         echo "${color2}*** Backup to CPHT cancelled${reset}"
