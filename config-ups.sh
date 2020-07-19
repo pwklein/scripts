199dfa9c9288c58244f8fe5ecaf4f32566252bbd
pwklein <philipp.klein@polytechnique.edu>
2020-07-19 23:46:53 +0200

    no artifical remote

--- config-ups.sh
+++ config-ups.sh
@@ -4,6 +4,6 @@ git_dir=${PWD##*/}
 
 git remote add gitups git@github.com:/pwklein/${git_dir}.git
 
-git remote add ups pwk@galilei:/srv/git/${git_dir}.git
-git remote set-url --add --push ups pwk@galilei:/srv/git/${git_dir}.git
-git remote set-url --add --push ups git@github.com:pwklein/${git_dir}.git
+#git remote add ups pwk@galilei:/srv/git/${git_dir}.git
+git remote set-url --add --push origin pwk@galilei:/srv/git/${git_dir}.git
+git remote set-url --add --push origin git@github.com:pwklein/${git_dir}.git
