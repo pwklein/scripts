e6d385cb0210743625768202e2ec7b72e7e1f7ca
pwklein <philipp.klein@polytechnique.edu>
2020-07-23 00:22:59 +0200

    nicer scripting

--- osync-run.sh
+++ osync-run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+notification () {
+		
+		if pgrep -x "dunst" > /dev/null; then
+				notify-send 'osync' "$1"
+		else
+				echo $1
+		fi
+}
+
+if pgrep -x "rsync" > /dev/null
+then
+		notification 'Already running'
+		exit 1
+fi
+
+notification 'started'
+
 sudo osync.sh ~/dotfiles/osync/osync.conf --errors-only --silent
+
+notification 'done'
