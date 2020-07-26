637e9bbcbe8c14c71b535fe5136890bbca1ed7d2
pwklein <philipp.klein@polytechnique.edu>
2020-07-26 16:54:28 +0200

    backup script correction

--- rsync_week_casimir.sh
+++ rsync_week_casimir.sh
@@ -2,11 +2,11 @@
 
 DAY=$(date +%d)
 ssh casimir8 << HERE
-if [ -e "$HOME/osync-backup/$DAY" ] ; then
-  rm -fr "$HOME/osync-backup/$DAY"
+if [ -e "/home/labcpht/pmc/philipp.klein/osync-backup/$DAY" ] ; then
+  rm -fr "/home/labcpht/pmc/philipp.klein/osync-backup/$DAY"
 fi
 HERE
 
-rsync -az --quiet --delete --inplace --backup --backup-dir=$DAY $HOME/osync casimir8:$HOME/osync-backup/
+rsync -az --quiet --delete --inplace --backup --backup-dir=$DAY $HOME/osync casimir8:/home/labcpht/pmc/philipp.klein/osync-backup/
 
 python ~/scripts/telegram_bot_text.py 'Done daily backup to casimir'
